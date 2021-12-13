dd3643c20ab0c11f5456948ce029c87ef83a2f7f
thanhbc <buichithanh24122000@gmail.com>
2021-12-13 09:55:26 +0700

    Add project files.

---
+++ PracticalEAP/Models/EAPPraticalContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace PracticalEAP.Models
+{
+    public partial class EAPPraticalContext : DbContext
+    {
+        public EAPPraticalContext(DbContextOptions<EAPPraticalContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; }
+
+   
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasKey(e => e.SutdentId)
+                    .HasName("PK__Employee__2780FD4C78A9637C");
+
+                entity.ToTable("Employee");
+
+                entity.Property(e => e.SutdentId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("SutdentID");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(1)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirtName)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.PhoneNumber)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .IsFixedLength(true);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ PracticalEAP/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace PracticalEAP.Models
+{
+    public partial class Employee
+    {
+        public int SutdentId { get; set; }
+        public string FirtName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
